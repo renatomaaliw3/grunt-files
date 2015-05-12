2fe347961160c31a072af515bc8f692ddfcc82b5
renatomaaliw3 <renatomaaliw3@yahoo.com>
2015-05-12 09:02:12 +0800

    Added Grunt-File (LESS)

---
+++ 01. gruntfile - LESS.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		less: {
+
+	  		development: {
+	    		
+	    		options: {
+	      		
+	      			paths: ["less"]
+	    		
+	    		},
+		    	files: {
+
+		      		"css/base.css": "less/base.less",
+		      		"css/layout.css": "less/layout.less",
+		      		"css/module.css": "less/module.less",
+		      		"css/state.css": "less/state.less",
+		      		"css/theme.css": "less/theme.less",
+		      		"css/theme-color.css": "less/theme-color.less",
+
+		    	}
+
+	  		}
+
+	  	},  //end less
+
+	  	watch: {
+
+	  		css: {
+
+	  			files: "less/*.less",
+	  			tasks: ["less"]
+
+	  		}
+
+	  	}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+}
